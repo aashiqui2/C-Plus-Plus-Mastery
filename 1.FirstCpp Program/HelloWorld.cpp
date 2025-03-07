01a26251187119e33aed827adf4a0d870fa9d9bb
Aashiqui <ashikmail2747@gmail.com>
2025-03-07 16:58:38 +0530

    Making Notes

---
+++ 1.FirstCpp Program/HelloWorld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout<<"hello world!";
+    // cout<<"hello world!";
+
+
+    // cout<<"hello world!"<<"\n";
+    // cout<<"hello world!";
+ 
+    //! \n is faster than endl;
+    // cout<<"hello world!"<<endl;
+    // cout<<"hello world!";
+
+
+    // cout<<"hello world "<< "by foolish programer "<<endl;
+    // cout<<"hello world\n from foolish programmer";
+    return 0;
+}
---
+++ 4.Variable/Variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // int a = 20;
+    // int b = 30;
+    // int sum = a + b;
+    // cout << sum;
+
+    // int a{2};
+    // int b{5};
+    // cout << a + b;
+    return 0;
+}
