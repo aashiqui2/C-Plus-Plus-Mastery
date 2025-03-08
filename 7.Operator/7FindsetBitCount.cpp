dac3a3cad140b344fcdda495a2672e38805063bd
Aashiqui <ashikmail2747@gmail.com>
2025-03-08 21:10:15 +0530

    Made modification

---
+++ 7.Operator/7FindsetBitCount.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int countSetBits(int n) {
+    int count = 0;
+    while (n) {
+        n &= (n - 1); // Clears the lowest set bit
+        count++;
+    }
+    return count;
+}
+int main()
+{
+    //   int n=256;
+    // int n = {15};
+    // cout << "The set bit count is : " << __builtin_popcount(n);
+
+    //! Brian Kernighan’s Algorithm (Efficient)
+    int num = 29; // Binary: 11101 (4 set bits)
+    std::cout << "Number of set bits: " << countSetBits(num) << std::endl;
+
+    return 0;
+}
